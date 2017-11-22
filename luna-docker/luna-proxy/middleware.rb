654906fea26e957f1aba989ca1990f64b447f00a
Richard Aberefa <ngrichyj4@gmail.com>
2017-11-22 12:34:45 -0700

    changes

--- luna-docker/luna-proxy/middleware.rb
+++ luna-docker/luna-proxy/middleware.rb
@@ -2,30 +2,30 @@ require File.expand_path('routes', File.dirname(__FILE__))
 require 'awesome_print'
 
 module Luna
-	class Middleware
-	  def initialize(app)
-	  	Luna::Routes.ping
-	    @app = app
-	  end
+  class Middleware
+    def initialize(app)
+      Luna::Routes.ping
+      @app = app
+    end
 
-	  #
+    #
     # Proxy request via reverse proxy
     #
     
-	  def call(env)	    
-	    proxy = Rack::ReverseProxy.new @app do
-	    	# Set :preserve_host to true globally (default is true already)
-			  reverse_proxy_options preserve_host: true, 
-			  verify_mode: OpenSSL::SSL::VERIFY_NONE, 
-			  	x_forwarded_headers: false
+    def call(env)     
+      proxy = Rack::ReverseProxy.new @app do
+        # Set :preserve_host to true globally (default is true already)
+        reverse_proxy_options preserve_host: true, 
+        verify_mode: OpenSSL::SSL::VERIFY_NONE, 
+          x_forwarded_headers: false
 
-			  # Forward the path /test* to http://example.com/test*
-			  #/^(.*)$/
-			  reverse_proxy /^(.*)$/, Luna::Routes.route( env )
-	    end
+        # Forward the path /test* to http://example.com/test*
+        #/^(.*)$/
+        reverse_proxy /^(.*)$/, Luna::Routes.route( env )
+      end
 
-	    ap env
-	    proxy.call(env)
-	  end
-	end
+      ap env
+      proxy.call(env)
+    end
+  end
 end
--- luna-docker/luna-proxy/routes.rb
+++ luna-docker/luna-proxy/routes.rb
@@ -1,38 +1,38 @@
 require 'rest-client'
 module Luna
-	class Routes
-		$api = 'http://example.com/'
-		class << self
-			#
-			# Fetch routes to proxy request to from remote 
-			# server based on hostname given
-			#
+  class Routes
+    $api = 'http://ee3e0735.ngrok.io/'
+    class << self
+      #
+      # Fetch routes to proxy request to from remote 
+      # server based on hostname given
+      #
 
-			def fetch (env)
-				json = RestClient.get $api +'v1/get_domain', { params: { host: env['HTTP_HOST'] } } rescue nil
-				json = JSON.parse json
-				json[:domain]
-			end
+      def fetch (env)
+        json = RestClient.get $api +'v1/get_domain', { params: { host: env['HTTP_HOST'] } } rescue nil
+        json = JSON.parse(json).symbolize_keys
+        json[:domain]
+      end
 
-			# 
-			# Used to ping remote server of upstart
-			# 
+      # 
+      # Used to ping remote server of upstart
+      # 
 
-			def ping
-				RestClient.get $api +'v1/upstart', { params: { keepalive: 1 } } rescue nil
-			end
+      def ping
+        RestClient.get $api +'v1/upstart', { params: { keepalive: 1 } } rescue nil
+      end
 
-			#
-			# Return which route to use 
-			#
+      #
+      # Return which route to use 
+      #
 
-			def route (env)
-				# http://blog.siami.fr/diving-in-rails-the-request-handling
-				# http://jmcglone.com
-				# fetch(env)
-				'http://risingstars2016.netlify.com/$1'
-			end
+      def route (env)
+        # http://blog.siami.fr/diving-in-rails-the-request-handling
+        # http://jmcglone.com
+        # fetch(env)
+        'http://risingstars2016.netlify.com/$1'
+      end
 
-		end
-	end
+    end
+  end
 end
